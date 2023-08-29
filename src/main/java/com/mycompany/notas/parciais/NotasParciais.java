120bdade9e2b83be39bd4e68e2fb637c67d8bcdf
Raul Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2023-08-28 22:13:25 -0300

    Update NotasParciais.java
    adicionados scanners

--- src/main/java/com/mycompany/notas/parciais/NotasParciais.java
+++ src/main/java/com/mycompany/notas/parciais/NotasParciais.java
@@ -1,11 +1,18 @@
 package com.mycompany.notas.parciais;
-
+import java.util.Scanner;
 public class NotasParciais {
 
     public static void main(String[] args) {
         NotasParciais2 result = new NotasParciais2();
-        result.nota01 = 7;
-        result.nota02 = 7;
+        Scanner snota = new Scanner(System.in);
+        System.out.println("Digite sua primeira nota!");
+        result.nota01 = (snota.nextInt());
+
+        Scanner snota2 = new Scanner(System.in);
+        System.out.println("Digite sua segunda nota!");
+        result.nota02 = (snota2.nextInt());
         result.resultfinal();
+        snota.close();
+        snota2.close();
     }
 }
